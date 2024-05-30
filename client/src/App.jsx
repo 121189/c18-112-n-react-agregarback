9e8a70f3a693d13720b549de968831bfbbbb57f7
Maite Canaviri <maite.c.luque@gmail.com>
2024-05-30 17:25:16 -0300

    Add Tailwind config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="text-4xl font-semibold text-violet-600">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
