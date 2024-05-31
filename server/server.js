1e7ec734373281415fded4d5bbf97853d010750e
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-31 01:30:03 -0400

    Agregar rutas de sesión para la autenticación de usuarios

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+require('dotenv').config();
 require('./config/database.config');
 app.use(cookieParser());
 app.use(express.json());
@@ -16,7 +16,11 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 //Routes
 const userRoutes = require('./routes/user.routes');
 app.use('/api/user', userRoutes);
+const sessionRoutes = require('./routes/session.routes');
+app.use('/api/session', sessionRoutes);
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
