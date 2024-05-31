13ac30456a9575746b04b9f14bf49426c5f5a4e4
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-31 01:54:36 -0400

    Update database name to "QueRápidasDB"

--- server/config/database.config.js
+++ server/config/database.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_NAME = "Cookingzone";
+const DB_NAME = "QueRápidasDB";
 
 mongoose.set('strictQuery', false);//importante
 mongoose.connect(`mongodb://127.0.0.1/${DB_NAME}`, {
