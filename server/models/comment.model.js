6bae534044ea3966a843a976c16943f1b373ef7a
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-06-06 01:48:30 -0400

    Agregar modelo de comentarios para almacenar comentarios en recetas

---
+++ server/models/comment.model.js
@@ -0,0 +1,20 @@
+const CommentSchema = new Schema({
+    content:{
+        type: String,
+        required: true,
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    recipe:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Recipes",
+        required: true,
+    },
+}, { timestamps: true }
+);
+
+const Comment = new mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
