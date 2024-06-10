cc02acc710025f3d8b26e747ad68646573486555
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-06-10 03:44:37 -0400

    Actualizar la funcionalidad de comentarios en el servidor

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -26,11 +26,28 @@ module.exports.getCommentsByRecipeId = async (req, res) => {
     }
 };
 
+//Update comment by id
+module.exports.updateComment = async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.id).populate("owner").populate("recipe");
+        if (req.body.userId.toString() === comment.owner._id.toString()) {
+            const updatedComment = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            res.status(200);
+            res.json(updatedComment);
+        } else {
+            res.status(403);
+            res.json({ message: "No tienes permiso para editar este comentario" });
+        }
+    } catch (error) {
+        res.status(500);
+        res.json(error);
+    }
+};
+
 // Delete a comment
 module.exports.deleteComment = async (req, res) => {
     try {
         const comment = await Comment.findById(req.params.id).populate("owner").populate("recipe");
-        console.log(comment);
         if (comment.recipe.owner.toString() === req.body.userId.toString() || req.body.userId.toString() === comment.owner._id.toString()) {
             await Comment.findByIdAndDelete(req.params.id); 
             res.status(200);
