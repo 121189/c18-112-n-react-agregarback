3123041ca1a21c9156a7443dd68c86c0a5611605
Maite Canaviri <maite.c.luque@gmail.com>
2024-06-12 17:51:50 -0300

    Fix text color

--- client/src/components/recipe/CommentsSection.jsx
+++ client/src/components/recipe/CommentsSection.jsx
@@ -6,7 +6,7 @@ const CommentsSection = ({ comments }) => {
     <div className="min-h-14 rounded-md bg-white p-6 shadow-md">
       <h2 className="mb-4 flex items-center gap-2">
         <svg
-          className="h-8 w-8"
+          className="h-8 w-8 text-gray-700"
           data-slot="icon"
           fill="none"
           strokeWidth="1.5"
@@ -21,7 +21,9 @@ const CommentsSection = ({ comments }) => {
             d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
           ></path>
         </svg>
-        <span className="text-2xl font-semibold">Comentarios</span>
+        <span className="text-2xl font-semibold text-gray-700">
+          Comentarios
+        </span>
       </h2>
       <form action="" className="mb-8">
         <div className="relative">
