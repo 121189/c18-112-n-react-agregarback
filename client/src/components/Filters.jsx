30658d233533a4aea235443261d7b0a6f61b9a13
Maite Canaviri <maite.c.luque@gmail.com>
2024-06-14 15:31:58 -0300

    Reset filter value

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,12 +1,41 @@
 import React, { useState } from "react";
 import Axios from "axios";
 
+const orderFormValues = {
+  titleAsc: {
+    orderBy: "title",
+    order: 1,
+  },
+  titleDesc: {
+    orderBy: "title",
+    order: -1,
+  },
+  durationAsc: {
+    orderBy: "duration",
+    order: 1,
+  },
+  durationDesc: {
+    orderBy: "duration",
+    order: -1,
+  },
+  noOrder: {
+    orderBy: "",
+    order: "",
+  },
+};
+
 const Filters = ({ query, setRecipes, page = "" }) => {
   const [maxDuration, setMaxDuration] = useState("");
   const [ingredientsQty, setIngredientsQty] = useState("");
   const [portionsQty, setPortionsQty] = useState("");
-  const [orderBy, setOrderBy] = useState("title");
-  const [order, setOrder] = useState(1);
+  const [orderBy, setOrderBy] = useState("noOrder");
+
+  const handleResetFilters = () => {
+    setMaxDuration("");
+    setIngredientsQty("");
+    setPortionsQty("");
+    setOrderBy("noOrder");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,8 +45,8 @@ const Filters = ({ query, setRecipes, page = "" }) => {
       maxDuration,
       ingredientsQty,
       portionsQty,
-      orderBy,
-      order,
+      orderBy: orderFormValues[orderBy].orderBy,
+      order: orderFormValues[orderBy].order,
     });
 
     try {
@@ -26,8 +55,8 @@ const Filters = ({ query, setRecipes, page = "" }) => {
         maxDuration,
         ingredientsQty,
         portionsQty,
-        orderBy,
-        order,
+        orderBy: orderFormValues[orderBy].orderBy,
+        order: orderFormValues[orderBy].order,
       });
 
       console.log(response.data);
@@ -55,6 +84,8 @@ const Filters = ({ query, setRecipes, page = "" }) => {
               value={maxDuration}
               id="maxDuration"
               name="maxDuration"
+              min={1}
+              max={30}
               type="number"
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
               placeholder="Introduce la duración máxima en minutos"
@@ -72,6 +103,8 @@ const Filters = ({ query, setRecipes, page = "" }) => {
               id="ingredientsQty"
               name="ingredientsQty"
               value={ingredientsQty}
+              min={1}
+              max={5}
               type="number"
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
               placeholder="Introduce la cantidad de ingredientes"
@@ -93,6 +126,7 @@ const Filters = ({ query, setRecipes, page = "" }) => {
               type="number"
               name="portionsQty"
               value={portionsQty}
+              min={1}
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
               placeholder="Introduce la cantidad de raciones"
               onChange={(e) => setPortionsQty(e.target.value)}
@@ -108,30 +142,11 @@ const Filters = ({ query, setRecipes, page = "" }) => {
             <select
               name="sortBy"
               id="status"
+              value={orderBy}
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
-              onChange={(e) => {
-                const value = e.target.value;
-
-                switch (value) {
-                  case "titleAsc":
-                    setOrderBy("title");
-                    setOrder(1);
-                    break;
-                  case "titleDesc":
-                    setOrderBy("title");
-                    setOrder(-1);
-                    break;
-                  case "durationAsc":
-                    setOrderBy("duration");
-                    setOrder(1);
-                    break;
-                  case "durationDesc":
-                    setOrderBy("duration");
-                    setOrder(-1);
-                    break;
-                }
-              }}
+              onChange={(e) => setOrderBy(e.target.value)}
             >
+              <option value="noOrder">Seleccione una opción</option>
               <option value="titleAsc">Título A - Z</option>
               <option value="titleDesc">Título Z - A</option>
               <option value="durationAsc">Duración Asc</option>
@@ -139,6 +154,13 @@ const Filters = ({ query, setRecipes, page = "" }) => {
             </select>
           </div>
         </div>
+        <button
+          type="button"
+          className="w-full rounded-lg bg-red-500 px-5 py-2.5 text-center text-sm font-medium text-white transition-colors hover:bg-red-400 focus:outline-none focus:ring-4 focus:ring-red-300 disabled:bg-red-300 disabled:shadow-none"
+          onClick={handleResetFilters}
+        >
+          Reset
+        </button>
         <button
           type="submit"
           /* disabled={isLoading} */
