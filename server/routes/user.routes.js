6089b7358e1559fb2b41b2018ef541b423c0cf39
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-06-14 03:43:10 -0400

    feat: Agregar soporte para subir imágenes en la ruta de actualización de usuario

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+
+// Multer config for images
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/'); // Guardar los archivos en la carpeta "uploads"
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname); // Mantener el nombre original del archivo
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 const userController = require('../controllers/user.controller');
 const { authenticate } = require('../config/jwt.config');
@@ -12,11 +27,11 @@ router.patch("/passwordReset", userController.passwordReset);
 router.post("", userController.createUser);
 router.get("", authenticate, userController.findAllUsers);
 router.post("/:id/:page?", authenticate, userController.findUser);
-router.put("/:id", authenticate, userController.updateUser);
+router.put("/:id", authenticate,upload.single('image'), userController.updateUser);
 router.delete("/:id", authenticate, userController.deleteUser);
 
 //Followers and followed controller
-router.post("/:id/follow", authenticate, userController.follow);
+router.post("/:id/follow/user", authenticate, userController.follow);
 router.delete("/:id/follow", authenticate, userController.unfollow);
 //Gets a user's favorites list
 router.get("/favorites", authenticate, userController.getFavorites);
