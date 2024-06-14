000e9114cf287053d09e4f224fc72b45d6720cf7
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-06-14 00:12:40 -0400

    Actualizar texto de presentación en la página de inicio

--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -14,25 +14,10 @@ export const HomeScreen = () => {
           <h1 className="mb-4 hidden text-center text-5xl font-semibold">
             QueRapida
           </h1>
-          <p className="mt-6 text-center text-lg text-gray-800">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
-            aliquid inventore pariatur et voluptates accusantium, saepe velit
-            modi sequi sit reiciendis. Atque, expedita sunt? Incidunt omnis
-            mollitia unde nobis nihil, dolore sapiente?
+          <p className="mt-6 text-center text-xl text-gray-800 font-semibold">
+            QUERAPIDA es una innovadora red social de cocina diseñada para
+            entusiastas culinarios de todos los niveles
           </p>
-          {/* <p className="mb-6 text-center">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit qui
-            reprehenderit molestiae, dolore amet, obcaecati aut quas doloremque
-            iure adipisci esse ullam. Molestiae, unde?
-          </p> */}
-          {/* <div className="flex justify-center">
-            <Link
-              to="/ultimas-recetas"
-              className="mr-4 rounded-lg bg-orange-500 px-5 py-2.5 text-center text-sm font-medium text-white transition-colors hover:bg-orange-400 focus:outline-none focus:ring-4 focus:ring-orange-300"
-            >
-              Explorar recetas
-            </Link>
-          </div> */}
         </div>
       </Container>
     </section>
